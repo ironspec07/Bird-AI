57ca9304ca43baf79c36e1dbe17005b5bf6efbe5
ironspec <ironspec07@gmail.com>
2023-09-04 23:01:12 +0530

    authentication system complete

--- bird-ai/app/(dashboard)/(routes)/dashboard/page.tsx
+++ bird-ai/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 const DashboardPage = () => {
     return (
+        <div>
         <p>Dashboard Page (protected)</p>
+        <UserButton afterSignOutUrl="/" />
+        </div>
     )
 }
 export default DashboardPage;
--- bird-ai/app/(landing)/page.tsx
+++ bird-ai/app/(landing)/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 const LandingPage = () => {
-    return(
-        <div>
-            Landing Page (unprotected)
-        </div>
-    );
-}
+  return (
+    <div>
+      Landing Page (unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>login</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default LandingPage;
