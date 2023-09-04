e47a9a17bead9327dcb3506e4634f1e3921b9ab9
ironspec <ironspec07@gmail.com>
2023-09-04 17:21:05 +0530

    installed clerk and created environment file

--- bird-ai/app/(dashboard)/(routes)/dashboard/page.tsx
+++ bird-ai/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,6 @@
-import { Button } from "@/components/ui/button"
-
 const DashboardPage = () => {
     return (
-        <Button variant="destructive" size="lg">Click Me</Button>
+        <p>Dashboard Page (protected)</p>
     )
 }
 export default DashboardPage;
