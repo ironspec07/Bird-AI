7ad9e4a05b9128d05234ca16a0da39c54b935581
ironspec <ironspec07@gmail.com>
2023-09-05 01:10:45 +0530

    sidebar complete

--- bird-ai/components/navbar.tsx
+++ bird-ai/components/navbar.tsx
@@ -1,17 +1,15 @@
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import { Menu } from "lucide-react";
+import MobileSidebar from "@/components/mobile-sidebar";
+
 const Navbar = () => {
-    return (
-        <div className="flex items-center p-4">
-            <Button variant="ghost" size="icon" className="md:hidden">
-                <Menu/>
-            </Button>
-            <div className="flex w-full justify-end">
-                <UserButton afterSignOutUrl="/"/>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="flex items-center p-4">
+        <MobileSidebar/>
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
--- bird-ai/components/sidebar.tsx
+++ bird-ai/components/sidebar.tsx
@@ -4,8 +4,57 @@ import Link from "next/link";
 import Image from "next/image";
 import {Montserrat} from "next/font/google";
 
-const montserrat = Montserrat
+import { cn } from "@/lib/utils";
+import{LayoutDashboard,MessageSquare,ImageIcon,VideoIcon,Music,Code,Settings} from "lucide-react";
 
+const montserrat = Montserrat({
+    weight: "600",
+    subsets: ["latin"]
+});
+
+const routes = [
+    {
+        label: "Dashboard",
+        icon:LayoutDashboard,
+        href: "/dashboard",
+        color:"text-sky-500",
+    },
+    {
+        label: 'Conversation',
+        icon: MessageSquare,
+        href: '/conversation',
+        color: "text-violet-500",
+      },
+      {
+        label: 'Image Generation',
+        icon: ImageIcon,
+        color: "text-pink-700",
+        href: '/image',
+      },
+      {
+        label: 'Video Generation',
+        icon: VideoIcon,
+        color: "text-orange-700",
+        href: '/video',
+      },
+      {
+        label: 'Music Generation',
+        icon: Music,
+        color: "text-emerald-500",
+        href: '/music',
+      },
+      {
+        label: 'Code Generation',
+        icon: Code,
+        color: "text-green-700",
+        href: '/code',
+      },
+      {
+        label: 'Settings',
+        icon: Settings,
+        href: '/settings',
+      },
+];
 const Sidebar = () => {
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
@@ -15,13 +64,29 @@ const Sidebar = () => {
                         <Image 
                         fill
                         alt="logo"
-                        src="/logo.png"
+                        src="/bird.png"
                         />
                     </div>
-                    <h1 className="text-2xl font-bold">
+                    <h1 className={cn("text-2xl font-bold", montserrat.className)}>
                         Bird-AI
                     </h1>
                 </Link>
+                <div className="space-y-1">
+                    {routes.map((route)=>(
+                        <Link
+                        href={route.href}
+                        key={route.href}
+                        className={cn(
+                            "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                          )}
+                        >
+                            <div className="flex item-center flex-1">
+                                <route.icon className={cn("h-5 w-5 mr-5" ,route.color)}/>
+                                {route.label}
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     );
