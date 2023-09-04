e320e05287d018f784c4c62b5647a1b5730e48a4
ironspec <ironspec07@gmail.com>
2023-09-04 17:03:29 +0530

    routing done

---
+++ bird-ai/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button"
+
+const DashboardPage = () => {
+    return (
+        <Button variant="destructive" size="lg">Click Me</Button>
+    )
+}
+export default DashboardPage;
