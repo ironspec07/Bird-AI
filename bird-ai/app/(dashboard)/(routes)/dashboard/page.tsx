f268ea3fdfe6aa2032a6febdd0ea71cbc2952815
ironspec <ironspec07@gmail.com>
2023-09-27 23:28:42 +0530

    dashboard complete

--- bird-ai/app/(dashboard)/(routes)/dashboard/page.tsx
+++ bird-ai/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,41 +1,86 @@
 "use client";
 
-import { MessageSquare } from "lucide-react";
+import {
+  ArrowRight,
+  Code,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  VideoIcon,
+} from "lucide-react";
 
 import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 const tools = [
-    {
-        label: "conversation",
-        icon:MessageSquare,
-        color:"text-violet-500",
-        bgColor:"bg-violet-500/10",
-        href:"/conversation"
-    }
-]
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image",
+  },
+  {
+    label: "Vedio Generation",
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/video",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+    href: "/code",
+  },
+];
 
 const DashboardPage = () => {
-    return (
-        <div>
-        <div className="mb-8 space-y-4">
-            <h2 className="text-2xl md:text-4xl font-bold text-center ">
-                Explore the power of AI
-            </h2>
-            <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-                chat with the smartest AI
-            </p>
-        </div>
-        <div className="px-4 md:px-20 lg:px-32 space-y-4">
-                {tools.map((tool) => (
-                    <Card 
-                    key={tool.href}
-                    className="p-4 border-black/5 flex item-center justify-between hover:shadow-md transition cursor-pointer"
-                    >
-
-                    </Card>
-                ))}
+  const router = useRouter();
+  return (
+    <div>
+      <div className="mb-8 space-y-4 md:ml-60">
+        <h2 className="text-2xl md:text-4xl font-bold text-center ">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          chat with the smartest AI
+        </p>
+      </div>
+      <div className="  px-4 md:pl-80 lg:pl-80 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-3 border-black/5 flex item-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex item-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-5 h-5", tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
             </div>
-        </div>
-    )
-}
+            <ArrowRight className="w-5 h-8" />
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default DashboardPage;
