716a17a00d020cb07a2f9b5389cae9561314d475
Ansh <ansh0kumar07@gmail.com>
2023-10-02 00:46:14 +0530

    Completed Conversation And Code Generation models

--- bird-ai/app/(dashboard)/(routes)/code/page.tsx
+++ bird-ai/app/(dashboard)/(routes)/code/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import * as z from "zod";
 import { Heading } from "@/components/Heading";
-import { MessageSquare } from "lucide-react";
+import { Code, Divide } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { formSchema } from "./constants";
 import {zodResolver} from "@hookform/resolvers/zod";
@@ -18,8 +18,9 @@ import { Loader } from "@/components/loader";
 import { cn } from "@/lib/utils";
 import { UserAvatar } from "@/components/user-avatar";
 import { BotAvatar } from "@/components/bot-avatar";
+import ReactMarkdown from "react-markdown";
 
-const ConversationPage = () => {
+const CodePage = () => {
     const router = useRouter();
     const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
 
@@ -39,7 +40,7 @@ const ConversationPage = () => {
                 content: values.prompt,
             };
             const newMessages = [...messages, userMessage];
-            const response = await axios.post("/api/conversation", {messages: newMessages,});
+            const response = await axios.post("/api/code", {messages: newMessages,});
 
             setMessages((current) => [...current, userMessage, response.data]);
             form.reset();
@@ -53,11 +54,11 @@ const ConversationPage = () => {
     return (
         <div>
           <Heading 
-           title="Conversation"
-           description="Our AI ChatBot"
-           icon={MessageSquare}
-           iconColor="text-violet-500"
-           bgColor="bg-violet-500/10"
+           title="Code Generation"
+           description="Generate code using descriptive text"
+           icon={Code}
+           iconColor="text-green-700"
+           bgColor="bg-green-700/10"
           />
           <div className="px-4 lg:px-8">
             <div>
@@ -84,7 +85,7 @@ const ConversationPage = () => {
                                     <Input 
                                     className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                                     disabled={isLoading}
-                                    placeholder="what can i tell?"
+                                    placeholder="ask me to write a code if you can't find it on stackoverflow"
                                     {...field}
                                     />
                                 </FormControl>
@@ -116,9 +117,21 @@ const ConversationPage = () => {
                         )}
                         >
                             {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                            <p className="text-sm">
-                            {message.content}
-                            </p>
+                            <ReactMarkdown
+                                components={{
+                                    pre: ({node, ...props}) => (
+                                        <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                                            <pre {...props} />
+                                        </div>
+                                    ),
+                                    code: ({node, ...props}) => (
+                                        <code className="bg-black/10 rounded-lg p-1" {...props}/>
+                                    )
+                                }}
+                                className="text-sm overflow-hidden leading-7"
+                            >
+                                {message.content || ""}
+                            </ReactMarkdown>
                         </div>
                     ))}
                 </div>
@@ -128,4 +141,4 @@ const ConversationPage = () => {
     );
 }
 
-export default ConversationPage;
+export default CodePage;
