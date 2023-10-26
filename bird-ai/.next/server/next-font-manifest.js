6c902a30307f3ebb8efcc99f5ef8aae31a365ccf
ironspec07 <ironspec07@gmail.com>
2023-10-26 16:59:11 +0530

    build

---
+++ bird-ai/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/adii/Bird-AI/bird-ai/app/layout":["static/media/c9a5bc6a7c948fb0-s.p.woff2"],"/home/adii/Bird-AI/bird-ai/app/(landing)/page":["static/media/36afab82570d46f2-s.p.woff2"],"/home/adii/Bird-AI/bird-ai/app/(dashboard)/layout":["static/media/36afab82570d46f2-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
