247bd596a4d81e612e7d8fcf072e09483f22a181
ironspec07 <ironspec07@gmail.com>
2023-10-26 17:13:00 +0530

    edited landing page

--- bird-ai/components/landing-hero.tsx
+++ bird-ai/components/landing-hero.tsx
@@ -29,7 +29,7 @@ export const LandingHero = () => {
         </div>
       </div>
       <div className="text-sm md:text-xl font-light text-zinc-400">
-        Create content using AI 10x faster.
+        Create content using AI.
       </div>
       <div>
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
@@ -40,9 +40,6 @@ export const LandingHero = () => {
             Start Generating For Free
           </Button>
         </Link>
-      </div>
-      <div className="text-zinc-400 text-xs md:text-sm font-normal">
-        No credit card required.
       </div>
     </div>
   );
